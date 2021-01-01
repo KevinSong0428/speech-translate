ca63cd9c92afd0ca0ed64d3d00c0bebffa0e10bb
KevinSong0428 <ks5138@nyu.edu>
2020-12-31 19:01:59 -0500

    convert from eng, korean to span, korean, eng

--- voice-voiceTranslate.py
+++ voice-voiceTranslate.py
@@ -1,22 +1,88 @@
 import speech_recognition as sr
 from googletrans import Translator
 import pytesseract
-import googletrans
 import pyttsx3  # text to speech
+from textblob import TextBlob
+import googletrans
 
 pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract"
 
 # for index, name in enumerate(sr.Microphone.list_microphone_names()):
 #     print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
 
+print(googletrans.LANGUAGES)
+
 # initialize var
 r = sr.Recognizer()
 m = sr.Microphone()
 engine = pyttsx3.init()
 
+en_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"
 kor_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_KO-KR_HEAMI_11.0"
 ch_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ZH-HK_TRACY_11.0"
 es_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ES-ES_HELENA_11.0"
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)  # changing index, changes voices. o for male, 1 for female
+
+engine.setProperty('rate', 125)  # changing rate to 150 (default is 200)
+
+
+def startConversion(path='sample.wav', lang="en-US", dst_lang='en-US'):
+    with sr.AudioFile(path) as source:
+        print('Fetching File: ' + path)
+        audio_text = r.record(source)
+        # recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
+        try:
+            # using google speech recognition
+            # convert wav file to text in that language
+            print('Converting audio transcripts into text ...')
+            text = r.recognize_google(audio_text, language=lang)
+            print(text)
+
+        except:
+            print('Sorry.. run again...')
+
+    translator = Translator()
+    if (dst_lang == "en-US" or dst_lang == ''):
+        # wants to translate to english
+        lang = TextBlob(text)
+        lang = lang.detect_language()
+        result = translator.translate(text, src = lang)
+        engine.setProperty('voice', en_voice_id)
+        engine.say(result.text)
+        engine.runAndWait()
+        print(result.text)
+    else:
+        if (dst_lang == "korean" or dst_lang == "ko"):
+            # wants to translate to korean
+            result = translator.translate(text, dest = dst_lang)
+            engine.setProperty('voice', kor_voice_id)
+            engine.say(result.text)
+            engine.runAndWait()
+            print(result.text)
+        elif (dst_lang == "spanish" or dst_lang == "es"):
+            # wants to translate to spanish
+            result = translator.translate(text, dest = dst_lang)
+            engine.setProperty('voice', es_voice_id)
+            engine.say(result.text)
+            engine.runAndWait()
+            print(result.text)
+
+
+def main():
+    startConversion("KOR_001.wav", "ko-KR", '')
+    print("\n")
+    startConversion("KOR_002.wav", "ko-KR", '')
+    print("\n")
+    startConversion("BBN.wav", '', 'korean')
+    print("\n")
+    startConversion("KOR_001.wav", "ko-KR", 'spanish')
+
+
+
+if __name__ == "__main__":
+    main()
 
 
 # only available for languages with 2 gender for voice
@@ -29,28 +95,21 @@ def change_voice():
         # changing voice to male
         engine.setProperty('voice', voice[0].id)
 
-
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)  # changing index, changes voices. o for male, 1 for female
-
-engine.setProperty('rate', 125)  # changing rate to 150 (default is 200)
-
-# default english
-engine.say("Hello")
-engine.runAndWait()
-
-# change_voice()
-
-# korean voice
-engine.setProperty('voice', kor_voice_id)
-engine.say("난 아주 많이 당신을 사랑합니다")
-engine.runAndWait()
-
-# chinese (cantonese) voice
-engine.setProperty('voice', ch_voice_id)
-engine.say("我非常爱你")
-engine.runAndWait()
-
+# # default english
+# engine.say("Hello")
+# engine.runAndWait()
+#
+# # change_voice()
+#
+# # korean voice
+# engine.setProperty('voice', kor_voice_id)
+# engine.say("난 아주 많이 당신을 사랑합니다")
+# engine.runAndWait()
+#
+# # chinese (cantonese) voice
+# engine.setProperty('voice', ch_voice_id)
+# engine.say("我非常爱你")
+# engine.runAndWait()
 
 # voices = engine.getProperty('voices')
 # for voice in voices:
@@ -67,35 +126,6 @@ engine.runAndWait()
 #     audio = r.record(source)
 # print(r.recognize_google(audio))
 
-def startConvertion(path='sample.wav', lang="en-US"):
-    with sr.AudioFile(path) as source:
-        print('Fetching File: ' + path)
-        audio_text = r.record(source)
-        # recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
-        try:
-            # using google speech recognition
-            print('Converting audio transcripts into text ...')
-            text = r.recognize_google(audio_text, language=lang)
-            print(text)
-
-        except:
-            print('Sorry.. run again...')
-    if (lang == "ko-KR"):
-        translator = Translator()
-        result = translator.translate(text)
-        print(result.text)
-
-
-def main():
-    startConvertion("KOR_001.wav", "ko-KR")
-    print("\n")
-    startConvertion("KOR_002.wav", "ko-KR")
-    print("\n")
-    startConvertion("BBN.wav", '')
-
-
-# if __name__ == "__main__":
-#     main()
 
 # print(googletrans.LANGUAGES)
 
